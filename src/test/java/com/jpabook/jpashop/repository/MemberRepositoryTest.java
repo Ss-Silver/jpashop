00b1d8c6059ef2155763051a122c552f11bf4a48
sangeun.seong <sangeun.seong@timf.co.kr>
2022-01-25 14:15:29 +0900

    first commit

---
+++ src/test/java/com/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.jpabook.jpashop.repository;
+
+//import static org.junit.Assert.*;
+
+import com.jpabook.jpashop.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+public class MemberRepositoryTest {
+
+}
